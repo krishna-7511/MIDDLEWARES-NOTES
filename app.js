21e24e5cfc835a8345a476bda18b1dbd919aa072
Krishna <gopikrishna7511@gmail.com>
2025-05-30 19:54:21 +0530

    save changes

--- app.js
+++ app.js
@@ -104,3 +104,120 @@ app.listen(8080,(req,res)=>{
     console.log("Connection established");
 })
 
+/*
+   
+const express=require("express");
+const app=express();
+
+const port=8080;
+
+
+// our first middleware
+/* app.use((req,res)=>{
+     let{q} = req.query;
+    console.log(q);
+    console.log("hi iam middleware");
+    res.send("middleware finished");
+});
+
+
+app.get("/",(req,res)=>{
+    res.send("Hi, I am root");
+});
+
+
+app.get("/random",(req,res)=>{
+    res.send("This is a random page");
+});
+
+app.listen(port,()=>{
+    console.log(`port is listened successfully ${port}`);
+}); */
+
+
+// next()
+
+app.use((req, res, next) => {
+  console.log("hi I am middleware");
+  next(); // let the request go to the next handler
+  console.log("This is after next()");   // // This is considered bad practice,because next is end of the code
+});
+
+app.use((req, res, next) => {
+  console.log("hi I am 2nd  middleware");
+  next(); // let the request go to the next handler
+});
+
+  /*
+    suppose we enter in localhost any unknown path but middleware runs
+  */
+
+app.get("/", (req, res) => {
+  res.send("Hi, I am root");
+});
+
+app.get("/random", (req, res) => {
+  res.send("This is a random page");
+});
+
+//creating utility middleware
+
+app.use((req,res,next)=>{
+    req.time = new Date(Date.now()).toString();  // The middleware executes before the response is sent to the client.
+    console.log(req.method, req.hostname, req.path,req.time);// that is the reason middleware written in last
+    next();
+});
+
+// exploring app.use for all un matched routes
+
+app.use("/random", (req, res,next) => {
+  res.send("I am only for random");
+  next();
+});
+
+app.use((req,res)=>{
+    res.send("page not found");  
+}); 
+
+//Api token as query string
+app.use("/api",(req,res, next)=>{
+    let {token} = req.query;
+    if(token==="giveaccess"){
+        next();
+    }
+    throw new ExpressError (401,"ACCESS DENIED!");
+});
+
+app.get("/api",(req,res)=>{
+    res.send("Data");
+});
+
+//passing multiple middlewares
+const checkToken = (req,res, next)=>{
+    let {token} = req.query;
+    if(token==="giveaccess"){
+        next();
+    }
+    throw new ExpressError (401,"ACCESS DENIED!");
+};
+
+app.get("/api",checkToken,(req,res)=>{
+    res.send("Data");
+});
+
+//Error handling (express default)
+app.get("/err",(req,res)=>{
+    abcd = abcd;
+});
+
+
+app.use((req,res)=>{
+    res.status(404).send("page not found");
+});
+
+app.listen(port, () => {
+  console.log(`port is listened successfully ${port}`);
+});
+
+
+*/
